0b6289bdd2b50cc201d56a31b2c4bb7784a39c7e
cancio <filipe.cancio@outlook.com>
2021-09-04 21:38:41 -0300

    Linking Components

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../Pages/Home';
 
 const Index: React.FC = props => {
       return (
         <>
-            <p>Routes</p>
+            <BrowserRouter>
+              <Switch>
+                <Route exact path="/" component={Home} />
+              </Switch>
+            </BrowserRouter>
         </>
       );
     }
