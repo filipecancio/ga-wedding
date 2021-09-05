96390089b8b3d9f87761f0e811899b3e362a88da
cancio <filipe.cancio@outlook.com>
2021-09-04 21:57:06 -0300

    Add css reset #2

--- src/Styles/index.tsx
+++ src/Styles/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Colors from './Colors';
+import Reset from './Reset';
 
 
 const Index: React.FC = props => {
       return (
         <>
             <Colors/>
+            <Reset/>
         </>
       );
     }
